80a2d7980f5921c8e81b430ee147872153f1a665
RocDev001 <104718437+RocDev001@users.noreply.github.com>
2025-07-21 23:50:43 +0100

    Update Hero.js
    update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => (
       <SectionText>
         "As a junior React developer, I bring enthusiasm, a strong foundation in JavaScript, and a willingness to learn and grow. With hands-on experience in building React applications, I am eager to contribute to projects, collaborate with team members, and expand my skills under mentorship. I am dedicated to delivering quality code, embracing feedback, and continuously improving to become a valuable asset to your development team."
       </SectionText>
-      <Button onClick={() => window.location = 'https://google.com'} >Learn More</Button>
+      <Button onClick={() => window.location = 'https://github.com/RocDev001'} >Learn More</Button>
     </LeftSection>
    </Section>
 );
